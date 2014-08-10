f9dee9e48a258733788aada61e377b2fa56e6dbb
iplusu <iplusu@gmail.com>
2014-08-10 00:53:40 -0400

    separate shield day

--- Functions/Extra/separateDshieldDay.m
+++ Functions/Extra/separateDshieldDay.m
@@ -3,14 +3,18 @@ function [nonIP, nonDshield, inIP, inDshield] = separateDshieldDay(usIP, matrix,
 load(['idetproject/dshield_may0' int2str(idx) '.mat']);
 
 nonDshield = [];
+nonIP = [];
 inDshield = [];
+inIP = [];
 
 [t, i] = ismember(usIP, dshIP);
 
 for idx = 1:length(usIP)
     if t(idx) == 1
         inDshield = [inDshield; matrix(idx)];
+        inIP = [inIP; usIP(idx)];
     else
         nonDshield = [nonDshield; matrix(idx)];
+        nonIP = [nonIP; usIP(idx)];
     end;
 end;
